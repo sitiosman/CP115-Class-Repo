04fb4b0cd502c45ab640c17675c35bb165fc82e4
Norman830 <normandanishrajimin@gmail.com>
2026-08-10 09:09:11 +0800

    labt test

--- labs/lab06/exercise1/exercise1.py
+++ labs/lab06/exercise1/exercise1.py
@@ -1,4 +1,12 @@
-# Escape Characters Exercise
-# Print the receipt shown in the lab, using \n for new lines and \t for columns.
-# Calculate every total, subtotal, and tax in your code. Do not type the money
-# amounts in directly. Show every amount with exactly two decimal places.
+#total for each item
+Total_coffee = 3.50 * 2
+Total_muffin = 2.10 * 3
+Total_water = 1.00 * 1
+Subtotal = float(Total_coffee + Total_muffin + Total_water)
+Tax = float(Subtotal * 0.06)
+Total = float(Subtotal + Tax)
+
+receipt = print(f
+    "========== RECEIPT ==========\n Item\t Price\t Qty\t Total\n Coffee\t $3.50\t 2\t ${Total_coffee}\n Muffin\t $2.10\t 3\t ${Total_muffin}\n Water\t $1.00\t 1\t ${Total_water} "
+
+)
---
+++ labs/lab06/lab-test-2.py
@@ -0,0 +1,5 @@
+Name = "Ahmad Bin Abu"
+Matric_No = "MS2025123499"
+print(f"Name : {Name}\t \t \t Matric. No : {Matric_No}\n")
+print(f" * \t \t \t \t *\n ** \t \t \t ** \n *** \t \t *** \n ****** \n *** \t \t *** \n ** \t \t \t ** \n * \t \t \t \t *\n")
+print(f"This is my \n \t second \n \t\t assignment \n I want 2x10 marks, which is 20  full marks")
