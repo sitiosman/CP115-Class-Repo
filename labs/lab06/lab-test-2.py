5a4ce2759545d94bb80573939bd72b2c35cad4d0
Norman830 <normandanishrajimin@gmail.com>
2026-08-10 09:13:05 +0800

    lab test 2

--- labs/lab06/lab-test-2.py
+++ labs/lab06/lab-test-2.py
@@ -2,4 +2,4 @@ Name = "Ahmad Bin Abu"
 Matric_No = "MS2025123499"
 print(f"Name : {Name}\t \t \t Matric. No : {Matric_No}\n")
 print(f" * \t \t \t \t *\n ** \t \t \t ** \n *** \t \t *** \n ****** \n *** \t \t *** \n ** \t \t \t ** \n * \t \t \t \t *\n")
-print(f"This is my \n \t second \n \t\t assignment \n I want 2x10 marks, which is 20  full marks")
+print(f"This is my \n \t second \n \t\t assignment \n I want 2x 10 marks, which is 20  full marks")
