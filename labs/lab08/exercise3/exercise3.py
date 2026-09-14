804a34589c335ee7d4d666b0839b176f1b3bd35a
Norman830 <normandanishrajimin@gmail.com>
2026-09-14 09:27:04 +0800

    lab 08 exercise 3

--- labs/lab08/exercise3/exercise3.py
+++ labs/lab08/exercise3/exercise3.py
@@ -2,7 +2,25 @@ day_type = input()
 show_time = int(input())
 customer_type = input()
 
+if day_type == "weekend":
+    if customer_type =="Adult":
+        base_price = 18
+    elif customer_type == "Child":
+        base_price = 12
+    elif customer_type == "Senior":
+        base_price = 15
+elif day_type == "weekday" :
+    if customer_type == "Adult":
+        base_price = 15
+    elif  customer_type == "Child":
+        base_price = 10
+    elif customer_type == "Senior":
+        base_price = 12
 
-
+if show_time > 18 :
+    final_price = base_price + 3
+else :
+    final_price = base_price
+        
 print(base_price)
 print(final_price)
