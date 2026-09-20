42e64fc2b3e122b345ad8a4530ef68ed82350296
Norman830 <normandanishrajimin@gmail.com>
2026-09-21 07:40:15 +0800

    lab

--- labs/lab08/exercise4/exercise4.py
+++ labs/lab08/exercise4/exercise4.py
@@ -1,6 +1,11 @@
 current_reading = int(input())
 previous_reading = int(input())
 
+consumption = current_reading - previous_reading
+
+if consumption <= 20 :
+    water_cost =
+
 
 
 print(consumption)
